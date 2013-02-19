aa044a7a777d047f58f2262c9b292032a2db72f5
david hodgetts <david.demainlalune@gmail.com>
2013-02-19 21:24:19 +0100

    connect to robofont

--- scripts/addImagesToCurrentFont.py
+++ scripts/addImagesToCurrentFont.py
@@ -1,95 +1,138 @@
 
-import os, re
+import os, re, sys
+from os.path import join, isfile
 
-try:
-	import mojo
-except:
-	print "not running in context of robofont"
+
 
 
 def extractKeyFromBaseName(basename):
-	key = None
-	key = extractKeyFromNumber(basename) or extractKeyFromLowerCase(basename) 
-	key = key or extractKeyFromUpperCase(basename) or extractKeyFromPunctuation(basename)
-	return key
+    key = None
+    key = extractKeyFromNumber(basename) or extractKeyFromLowerCase(basename) 
+    key = key or extractKeyFromUpperCase(basename) or extractKeyFromPunctuation(basename)
+    return key
 
 def extractKeyFromNumber(basename):
-	key = None
-	num_re = re.compile("^num-(\d)$", re.IGNORECASE)
-	numbers = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four",
-				"5": "five", "6": "six", "7": "seven", "8": "eight", "9":"nine"}
+    key = None
+    num_re = re.compile("^num-(\d)$", re.IGNORECASE)
+    numbers = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four",
+                "5": "five", "6": "six", "7": "seven", "8": "eight", "9":"nine"}
 
-	m = num_re.match(basename)
+    m = num_re.match(basename)
 
-	if m:
-		key = numbers[m.group(1)]
+    if m:
+        key = numbers[m.group(1)]
 
-	return key
+    return key
 
 def extractKeyFromBaseNameAndRegex(basename, regex):
-	key = None
-	
-	m = regex.match(basename)
+    key = None
+    
+    m = regex.match(basename)
 
-	if m:
-		key = m.group(1)
+    if m:
+        key = m.group(1)
 
-	return key
+    return key
 
 def extractKeyFromLowerCase(basename):
-	
-	regex = re.compile("^lc-([a-z])$")
+    
+    regex = re.compile("^lc-([a-z])$")
 
-	return extractKeyFromBaseNameAndRegex(basename, regex)
+    return extractKeyFromBaseNameAndRegex(basename, regex)
 
 def extractKeyFromUpperCase(basename):
-	
-	regex = re.compile("^UC-([A-Z])$")
+    
+    regex = re.compile("^UC-([A-Z])$")
 
-	return extractKeyFromBaseNameAndRegex(basename, regex)
+    return extractKeyFromBaseNameAndRegex(basename, regex)
 
 
 def extractKeyFromPunctuation(basename):
-	
-	regex = re.compile("^punct-([a-z]{2,})$")
+    
+    regex = re.compile("^punct-([a-z]{2,})$")
 
-	return extractKeyFromBaseNameAndRegex(basename, regex)
+    return extractKeyFromBaseNameAndRegex(basename, regex)
 
-	
-	
+    
+    
 
 
-# parseFileList:
+# parseFiles:
 # given a list of files (fullpath string representation)
 # return a dictionary where key is glyph name and value is full path to image
 # for all valid images in listing
-def parseFileList(fileList):
+def parseFiles(pathToDir, fileList):
 
-	result = {}
-
-
-	# convert file of fullpath string to list of (fullPath, basename, extension)
-	# TODO extract
-	# TODO use dictionary or named tupple
-	fileInfos = map (lambda fullPath: (fullPath, 
-										 os.path.splitext(os.path.basename(fullPath))[0], 
-										 os.path.splitext(os.path.basename(fullPath))[1][1:]),
-						fileList)
+    result = {}
 
 
-	valid_extensions = ['jpg', 'png', 'tif']
-
-	invalidExtensionsRemoved = filter(lambda (_, __, extension): extension.lower() in valid_extensions, fileInfos)
-
-	for fullpath, basename, _ in invalidExtensionsRemoved:
-		key = extractKeyFromBaseName(basename)
-		if key:
-			result[key] = fullpath
-	
+    # convert file names to (basename, extension)
+    # TODO extract
+    # TODO use dictionary or named tupple
+    fileInfos = map (lambda fileName: ( os.path.splitext(os.path.basename(fileName))[0], 
+                                        os.path.splitext(os.path.basename(fileName))[1][1:]),
+                        fileList)
 
 
-	return result
+    valid_extensions = ['jpg', 'png', 'tif']
 
+    invalidExtensionsRemoved = filter(lambda (_, extension): extension.lower() in valid_extensions, fileInfos)
+
+    for basename, extension in invalidExtensionsRemoved:
+        key = extractKeyFromBaseName(basename)
+        if key:
+            result[key] = join(pathToDir, basename) + '.' + extension
+    
+
+
+    return result
+
+
+
+def addImages(currentFont, dictOfImages):
+    for glyphName, imagePath in dictOfImages.items():
+        try:
+            glyph = currentFont.getGlyph(glyphName)
+        except:
+            print "invalid glyph name -> %s" % glyphName
+            continue
+
+        glyph.addImage(imagePath)
+
+        if glyph.image:
+            print "image load succesful for glyph %s" % glyphName 
+        else:
+            print "image load failed for glyph %s and path %s" % (glyphName, imagePath)
+
+def main():
+    currentFont = CurrentFont()
+    if currentFont == None:
+        print "no font is open, aborting"
+        return
+
+    folder_path = getFolder()
+    if folder_path:
+        pathToDir = folder_path[0]
+        files = [ f for f in os.listdir(pathToDir) if isfile(join(pathToDir,f)) and f[0] != '.']
+        dictOfImages = parseFiles(pathToDir, files)
+        addImages(currentFont, dictOfImages)
+
+        print "all done"
+    else:
+        print "operation canceled"
+
+runMain = True
+
+try:
+    import mojo    
+    from vanilla.dialogs import getFolder
+    
+except:
+    print "not running in context of robofont"
+    runMain = False
+
+if runMain:
+    main()
 
 
 
--- tests/test_addImagesToCurrentFont.py
+++ tests/test_addImagesToCurrentFont.py
@@ -5,54 +5,57 @@ scripts_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 's
 
 sys.path.append(scripts_dir)
 
-from addImagesToCurrentFont import parseFileList
+from addImagesToCurrentFont import parseFiles
 import unittest
 
 
 
-# Here's our "unit tests".
 class TestParseDirectory(unittest.TestCase):
 
+    fullPath = "/home/user/robo/images/"
+
     def test_itReturnsADictionary(self):
-        self.failUnless( isinstance(parseFileList([]), dict))
+        result = parseFiles(self.fullPath, [])
+        self.assertTrue( isinstance(parseFiles(self.fullPath,[]), 
+                                    dict))
 
 
     def test_itFiltersInvalidFileExtensions(self):
-    	fileList = ["/home/user/robo/images/lc-b.txt",
-    				"/home/user/robo/images/lc-a.doc",
-    				"/home/user/robo/images/lc-a.ai"]
-    	result = parseFileList(fileList)
+    	fileList = ["lc-b.txt",
+    				"lc-a.doc",
+    				"lc-a.ai"]
+    	result = parseFiles(self.fullPath, fileList)
     	self.assertEquals(len(result), 0)
 
 
     def test_itHandlesNumbers(self):
-        fileList = ["/home/user/robo/images/num-0.png",
-                    "/home/user/robo/images/num-10.png"]
-        result = parseFileList(fileList)
+        fileList = ["num-0.png",
+                    "num-10.png"]
+        result = parseFiles(self.fullPath, fileList)
         self.assertEquals(result, {"zero": "/home/user/robo/images/num-0.png"})
         
 
     def test_itHandlesLowerCaseLetters(self):
-        fileList = ["/home/user/robo/images/lc-b.png",
-                    "/home/user/robo/images/lC-A.png",
-                    "/home/user/robo/images/lc-bob.png"]
-        result = parseFileList(fileList)
+        fileList = ["lc-b.png",
+                    "lC-A.png",
+                    "lc-bob.png"]
+        result = parseFiles(self.fullPath, fileList)
         self.assertEquals(result, {"b": "/home/user/robo/images/lc-b.png"})
 
     def test_itHandlesUpperCaseLetters(self):
-        fileList = ["/home/user/robo/images/UC-B.png",
-                    "/home/user/robo/images/uC-C.png",
-                    "/home/user/robo/images/UC-BOB.png"]
-        result = parseFileList(fileList)
+        fileList = ["UC-B.png",
+                    "uC-C.png",
+                    "UC-BOB.png"]
+        result = parseFiles(self.fullPath, fileList)
         self.assertEquals(result, {"B": "/home/user/robo/images/UC-B.png"})
 
 
     # handling of punctuation leads to introduction of invalid glyph names
     # see bulubu
     def test_itHandlesPunctuation(self):
-        fileList = ["/home/user/robo/images/punct-ampersand.png",
-                    "/home/user/robo/images/punct-bulubu.png"]
-        result = parseFileList(fileList)
+        fileList = ["punct-ampersand.png",
+                    "punct-bulubu.png"]
+        result = parseFiles(self.fullPath, fileList)
         self.assertEquals(result, {"ampersand": "/home/user/robo/images/punct-ampersand.png",
                                     "bulubu": "/home/user/robo/images/punct-bulubu.png" })
 
