1d27116334b875c5fe1a082b00a826b5d3d9dc8a
david hodgetts <david.demainlalune@gmail.com>
2013-02-19 11:00:19 +0100

    extract key from basename

--- scripts/addImagesToCurrentFont.py
+++ scripts/addImagesToCurrentFont.py
@@ -1,5 +1,5 @@
 
-import os
+import os, re
 
 try:
 	import mojo
@@ -7,9 +7,64 @@ except:
 	print "not running in context of robofont"
 
 
-valid_extensions = ['jpg', 'png', 'tif']
+def extractKeyFromBaseName(basename):
+	key = None
+	key = extractKeyFromNumber(basename) or extractKeyFromLowerCase(basename) 
+	key = key or extractKeyFromUpperCase(basename) or extractKeyFromPunctuation(basename)
+	return key
+
+def extractKeyFromNumber(basename):
+
+	key = None
+	num_re = re.compile("^num-(\d)$", re.IGNORECASE)
+
+	numbers = {"0": "zero", "1": "one", "2": "two", "3": "three", "4": "four",
+				"5": "five", "6": "six", "7": "seven", "8": "eight", "9":"nine"}
+
+	m = num_re.match(basename)
+
+	if m:
+		key = numbers[m.group(1)]
+
+	return key
+
+def extractKeyFromLowerCase(basename):
+	key = None
+	num_re = re.compile("^lc-([a-z])$")
+
+	m = num_re.match(basename)
+
+	if m:
+		key = m.group(1)
+
+	return key
+
+def extractKeyFromUpperCase(basename):
+	key = None
+	num_re = re.compile("^UC-([A-Z])$")
+
+	m = num_re.match(basename)
+
+	if m:
+		key = m.group(1)
+
+	return key
 
 
+def extractKeyFromPunctuation(basename):
+	key = None
+	num_re = re.compile("^punct-([a-z]{2,})$")
+
+	m = num_re.match(basename)
+
+	if m:
+		key = m.group(1)
+
+	return key
+	
+
+
+# parseFileList:
 # given a list of files (fullpath string representation)
 # return a dictionary where key is glyph name and value is full path to image
 # for all valid images in listing
@@ -27,11 +82,15 @@ def parseFileList(fileList):
 						fileList)
 
 
+	valid_extensions = ['jpg', 'png', 'tif']
 
 	invalidExtensionsRemoved = filter(lambda (_, __, extension): extension.lower() in valid_extensions, fileInfos)
 
-	for fullPath, baseName, extension in invalidExtensionsRemoved:
-		result[baseName] = fullPath
+	for fullpath, basename, _ in invalidExtensionsRemoved:
+		key = extractKeyFromBaseName(basename)
+		if key:
+			result[key] = fullpath
+	
 
 
 	return result
--- tests/test_addImagesToCurrentFont.py
+++ tests/test_addImagesToCurrentFont.py
@@ -19,15 +19,42 @@ class TestParseDirectory(unittest.TestCase):
 
     def test_itFiltersInvalidFileExtensions(self):
     	fileList = ["/home/user/robo/images/lc-b.txt",
-    				"/home/user/robo/images/lc-a.jpg",
-    				"/home/user/robo/images/lc-a.JpG"]
+    				"/home/user/robo/images/lc-a.doc",
+    				"/home/user/robo/images/lc-a.ai"]
     	result = parseFileList(fileList)
-    	print result
-    	self.failUnless(len(result) == 1)
+    	self.assertEquals(len(result), 0)
 
 
+    def test_itHandlesNumbers(self):
+        fileList = ["/home/user/robo/images/num-0.png",
+                    "/home/user/robo/images/num-10.png"]
+        result = parseFileList(fileList)
+        self.assertEquals(result, {"zero": "/home/user/robo/images/num-0.png"})
+        
+
+    def test_itHandlesLowerCaseLetters(self):
+        fileList = ["/home/user/robo/images/lc-b.png",
+                    "/home/user/robo/images/lC-A.png",
+                    "/home/user/robo/images/lc-bob.png"]
+        result = parseFileList(fileList)
+        self.assertEquals(result, {"b": "/home/user/robo/images/lc-b.png"})
+
+    def test_itHandlesUpperCaseLetters(self):
+        fileList = ["/home/user/robo/images/UC-B.png",
+                    "/home/user/robo/images/uC-C.png",
+                    "/home/user/robo/images/UC-BOB.png"]
+        result = parseFileList(fileList)
+        self.assertEquals(result, {"B": "/home/user/robo/images/UC-B.png"})
 
 
+    # handling of punctuation leads to introduction of invalid glyph names
+    # see bulubu
+    def test_itHandlesPunctuation(self):
+        fileList = ["/home/user/robo/images/punct-ampersand.png",
+                    "/home/user/robo/images/punct-bulubu.png"]
+        result = parseFileList(fileList)
+        self.assertEquals(result, {"ampersand": "/home/user/robo/images/punct-ampersand.png",
+                                    "bulubu": "/home/user/robo/images/punct-bulubu.png" })
 
 
 def main():
