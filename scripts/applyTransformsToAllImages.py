8a0fe9b0f64590a5d8305d57aeead1dc4bfb5a12
david hodgetts <david.demainlalune@gmail.com>
2013-03-29 16:54:10 +0100

    quickfix applyTransformToAllImages: support target layer

--- scripts/applyTransformsToAllImages.py
+++ scripts/applyTransformsToAllImages.py
@@ -1,13 +1,41 @@
 import mojo
 
+TARGET_LAYER_NAME = "imported_images"
+
+def applyImageTransFormToAllImages(sourceGlyph, targetLayer):
+
+
+    sourceGlyphWithTargetLayer = sourceGlyph.getLayer(targetLayer)
+
+    if (sourceGlyphWithTargetLayer.image):
+        sourceImageTransformMatrix = sourceGlyphWithTargetLayer.image.transformMatrix
+        print "sourceImageTransformMatrix",  sourceImageTransformMatrix
+    else:
+        print "no image on current glyph", sourceGlyphWithTargetLayer
+        return
+
+
+    for glyph in CurrentFont():
+
+        destinationGlyph = glyph.getLayer(targetLayer)
+        if destinationGlyph == sourceGlyphWithTargetLayer:
+            continue
+        if destinationGlyph.image:
+            destinationGlyph.image.transform(sourceImageTransformMatrix)
+            print "applying (not copying) matrix from glyph %s to glyph %s" % (sourceGlyphWithTargetLayer, destinationGlyph)
+            print "destinationTransformMatrix", destinationGlyph.image.transformMatrix
+
+    print "all done"
+
+
+# main
+
 sourceGlyph = CurrentGlyph()
 
-imageTransformMatrix = sourceGlyph.image.transformMatrix
+if sourceGlyph == None:
+    print "no current glyph accessible, script aborting"
+else:
+    applyImageTransFormToAllImages(sourceGlyph, TARGET_LAYER_NAME)
 
-print imageTransformMatrix
 
-for g in CurrentFont():
-   if g == sourceGlyph:
-       continue
-   if g.image:
-       g.image.transform(imageTransformMatrix)
+
